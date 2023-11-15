802e0bd71cf5e46f85ec0786cee74e0827ffb3a9
Izuho Fujiwara <fujishan2212@gmail.com>
2023-11-15 22:33:22 +0900

    fix

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     });
 });
+
+const setFillHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', setFillHeight);
+
+setFillHeight();
