7728cb155a616d138055e68e8a17fe15827f51eb
Izuho Fujiwara <fujishan2212@gmail.com>
2023-11-15 22:03:48 +0900

    fix

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startCameraButton = document.getElementById('startCameraButton');
-    const startCountdownButton = document.getElementById('startCountdownButton');
-    const countdownElement = document.getElementById('countdown');
+    const container = document.getElementById('container');
+    const startButton = document.getElementById('startButton');
     const camera = document.getElementById('camera');
+    const textArea = document.getElementById('textArea');
 
-    startCameraButton.addEventListener('click', function() {
+    startButton.addEventListener('click', function() {
         navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } })
             .then(function (stream) {
                 camera.srcObject = stream;
-                startCameraButton.style.display = 'none';
-                startCountdownButton.style.display = 'block'; // PUSHボタンを表示
+                camera.style.display = 'block';
+                startButton.style.display = 'none';
+                container.style.backgroundColor = 'white';
+                textArea.style.display = 'block';
             })
             .catch(function (error) {
                 console.error("メディアデバイスの取得に失敗しました。", error);
             });
     });
-
-    startCountdownButton.addEventListener('click', function() {
-        startCountdownButton.style.display = 'none'; // PUSHボタンを隠す
-        startCountdown();
-    });
-
-    function startCountdown() {
-        let countdown = 5;
-        countdownElement.textContent = countdown;
-        countdownElement.style.display = 'block';
-
-        const countdownInterval = setInterval(function () {
-            countdown--;
-            countdownElement.textContent = countdown;
-
-            if (countdown <= 0) {
-                clearInterval(countdownInterval);
-                countdownElement.style.display = 'none';
-                camera.style.display = 'block';
-            }
-        }, 1000);
-    }
 });
