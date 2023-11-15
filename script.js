636daee0e90b437756dbf83e4f30629a35b29a35
Izuho Fujiwara <fujishan2212@gmail.com>
2023-11-15 21:28:41 +0900

    fix

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startButton = document.getElementById('startButton');
+    const startCameraButton = document.getElementById('startCameraButton');
+    const startCountdownButton = document.getElementById('startCountdownButton');
+    const countdownElement = document.getElementById('countdown');
     const camera = document.getElementById('camera');
 
-    startButton.addEventListener('click', function() {
+    startCameraButton.addEventListener('click', function() {
         navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } })
             .then(function (stream) {
                 camera.srcObject = stream;
-                camera.style.display = 'block';
-                startButton.style.display = 'none';
+                startCameraButton.style.display = 'none';
+                startCountdownButton.style.display = 'block'; // PUSHボタンを表示
             })
             .catch(function (error) {
                 console.error("メディアデバイスの取得に失敗しました。", error);
             });
     });
+
+    startCountdownButton.addEventListener('click', function() {
+        startCountdownButton.style.display = 'none'; // PUSHボタンを隠す
+        startCountdown();
+    });
+
+    function startCountdown() {
+        let countdown = 5;
+        countdownElement.textContent = countdown;
+        countdownElement.style.display = 'block';
+
+        const countdownInterval = setInterval(function () {
+            countdown--;
+            countdownElement.textContent = countdown;
+
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
+                countdownElement.style.display = 'none';
+                camera.style.display = 'block';
+            }
+        }, 1000);
+    }
 });
